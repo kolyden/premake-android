2416a3268ebb6617452ce052bc16889c31cd8b9d
Denis <kolyden@gmail.com>
2020-04-09 12:36:11 +0200

    firt commit

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+--local NDK_PATH = os.getenv("VS_NdkRoot")
+
+workspace "AndroidNative"
+	system "android"
+	cppdialect "c++14"
+	architecture "ARM"
+	location "project"
+	targetdir "bin/%{cfg.buildcfg}-%{cfg.platform}"
+
+	configurations { "Debug", "Release" }
+	platforms { "ARM", "ARM64" }
+
+	toolchainversion "5.0"
+	stl "libc++"
+	thumbmode "thumb"
+
+	startproject "AndroidNative.Packaging"
+
+project "AndroidNative.NativeActivity"
+	kind "SharedLib"
+	files "src/**"
+	removefiles "src/pch.h"
+	pchheader "../src/pch.h"
+	includedirs {
+		"$(StlIncludeDirectories)",
+		"$(Sysroot)/usr/include",
+		"$(Sysroot)/usr/include/$(AndroidHeaderTriple)",
+		"$(VS_NdkRoot)/sources/android/support/include",
+		"$(VS_NdkRoot)/sources/android/native_app_glue",
+    }
+    links { "GLESv1_CM", "EGL" }
+
+project "AndroidNative.Packaging"
+	kind "Packaging"
+	androidapplibname "AndroidNative.NativeActivity"
+	links "AndroidNative.NativeActivity"
+	files {
+		"res/**",
+		"AndroidManifest.xml",
+		"build.xml",
+		"project.properties"
+	}
